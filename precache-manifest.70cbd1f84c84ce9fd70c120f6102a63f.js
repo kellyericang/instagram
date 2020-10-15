5e0c23295c0aaef9e13a3068a162f05629e51baa
Kelly Ng <kelly.ng1@outlook.com>
2020-10-15 04:19:21 -0400

    Updates

---
+++ precache-manifest.70cbd1f84c84ce9fd70c120f6102a63f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ec40782fe3fb12d37cebd862e80d8f",
+    "url": "/instagram/index.html"
+  },
+  {
+    "revision": "ead819d931210797498e",
+    "url": "/instagram/static/css/main.6c13052c.chunk.css"
+  },
+  {
+    "revision": "f691c8a6aac7e5d7465e",
+    "url": "/instagram/static/js/2.3e90091b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/instagram/static/js/2.3e90091b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ead819d931210797498e",
+    "url": "/instagram/static/js/main.2eb8c361.chunk.js"
+  },
+  {
+    "revision": "060ad16ba7f5db70f5a5",
+    "url": "/instagram/static/js/runtime-main.a87bf05b.js"
+  },
+  {
+    "revision": "b0db68f3bc234fa7810842f4a70bb6a8",
+    "url": "/instagram/static/media/jellyfish.b0db68f3.jpg"
+  }
+]);
